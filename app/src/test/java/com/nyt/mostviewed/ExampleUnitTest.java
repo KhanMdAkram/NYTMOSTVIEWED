12e58612d694343544f89831c8fac0dd405251fe
khanak7 <md.akram.khan@intl.verizon.com>
2018-11-20 17:47:03 +0530

    Code Refracrtor.

--- app/src/test/java/com/nyt/mostviewed/ExampleUnitTest.java
+++ app/src/test/java/com/nyt/mostviewed/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package hud.veizon.com.myapplication;
+package com.nyt.mostviewed;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
